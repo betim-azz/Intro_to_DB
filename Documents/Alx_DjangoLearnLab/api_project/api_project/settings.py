a16349a8584b6393919f4128099905eb02dd6a75
bethelehem Melaku <betim7995@gmail.com>
2026-02-08 07:58:22 -0800

    Restarted project for Task 0

--- Documents/Alx_DjangoLearnLab/api_project/api_project/settings.py
+++ Documents/Alx_DjangoLearnLab/api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yv5dc67+s5j92g1(ajb&wgzk!69@p_h%rj(kk%#(903dok&s_i'
+SECRET_KEY = 'django-insecure-7tdw@5lse=#b3wf1v&yg43=^$-#a-57gd2&j91(prs%jwdze$c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
